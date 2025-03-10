66bc08615467c12d9d273659b9fa4f0abc8996cd
Joellindberg1 <Joellindberg23@gmail.com>
2025-03-10 14:57:55 +0100

    Footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useLocation } from "react-router-dom"
+
+interface FooterProps {
+  className?: string
+}
+
+const Footer: React.FC<FooterProps> = ({ className }) => {
+    const location = useLocation();
+
+    const patch = "1.1.0"
+    const defaultText = "© 2024 Civ7 Drafter. All rights reserved."
+    const patchText = `Updated until patch ${patch}` 
+    const draftComplete = "Thanks for using Civ 7 Drafter, please leave a comment and rating so we can continue developing towards a better drafter."
+
+    return (
+        <footer className={`footer ${className}`}>
+            <p>{defaultText}</p>
+            <p>{patchText}</p>
+            {location.pathname === "/FinalDraft" && <p>{draftComplete}</p>} 
+        </footer>
+    );
+};
+
+
+export default Footer;
