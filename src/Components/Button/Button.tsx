a186261a4baccc0e682ec0a6d7a10cb4bb342fa4
[Joel LIndberg] <[Joellindberg23@gmail.com]>
2025-03-10 14:58:04 +0100

    Button

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick, className = "" }) => {
+  return (
+    <button 
+      onClick={onClick} 
+      className={`btn ${className}`}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
