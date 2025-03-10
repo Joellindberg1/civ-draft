c944ed8e2c2dd8bee775590ca9f2a319d772d086
[Joel LIndberg] <[Joellindberg23@gmail.com]>
2025-03-10 14:57:44 +0100

    router input

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 //React
-// import { useState } from 'react'
+// import react from 'react'
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 //Components
+
 
 //Styles
 import './App.scss'
+
 //Pages
+import Home from "./pages/Home"
+import Setup from "./pages/Setup"
+import BanPhaseLeaders from "./pages/BanPhaseLeaders"
+import BanPhaseCivs from "./pages/BanPhaseCivs"
+import FinalDraft from "./pages/FinalDraft"
 
 //Types
+
 
 //Interfaces
 
 
-function App() {
 
+const App: React.FC = () => {
   return (
-    <>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/setup" element={<Setup />} />
+        <Route path="/banPhaseLeaders" element={<BanPhaseLeaders />} />
+        <Route path="/banPhaseCivs" element={<BanPhaseCivs />} />
+        <Route path="/FinalDraft" element={<FinalDraft />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App
